0498705f5c983631b01c9799957f012fb39b988b
poovarasanparamasivam <poovarasanparamasivam@outlook.com>
2024-02-05 19:09:46 +0530

    Initial deploy

---
+++ src/main/java/com/poovarasan/CCSS/CcssApplication.java
@@ -0,0 +1,13 @@
+package com.poovarasan.CCSS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcssApplication.class, args);
+	}
+
+}
